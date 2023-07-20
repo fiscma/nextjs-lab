a0b78fdc3724748beed95f5500ff2f3bdd9dee31
horn1347 <horn1347@users.noreply.github.com>
2023-07-20 21:18:04 +0200

    looks nice retro

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export async function Blog() {
         </h2>
       </div>
       <div className="p-2">
-        <p className=""> 
+        <p className="text-sm"> 
           Content
         </p>
       </div>
@@ -25,13 +25,13 @@ export async function Blog() {
 export function About() {
   return (
     <div className="flex flex-col">
-      <div className="p-2" font-bold>
+      <div className="p-2 font-bold" font-bold>
         Whoami?
       </div>
-      <div className="p-2" font-bold>
+      <div className="p-2 font-bold" font-bold>
         What am i doing here?
       </div>
-      <div className="p-2" font-bold>
+      <div className="p-2 font-bold" font-bold>
         How to get in touch?
       </div>
     </div>
@@ -42,11 +42,11 @@ export default function Home() {
   return (
     <main >
       <div className="flex flex-row font-mono  p-4 m-4 text-green-600 bg-black  space-x-2">
-        <div id="blog" className="basis-2/3  border-dotted border-2 rounded-md border-inherit border-green-800">
+        <div id="blog" className="basis-2/3  border-solid border-2 rounded-md border-inherit border-green-800">
           <Blog/>
         </div>
-        <div id="about" className="basis-1/2 border-dotted border-2 rounded-md border-green-800">
-            <About/>
+        <div id="about" className="basis-1/2 border-solid border-2 rounded-md border-green-800">
+          <About/>
         </div>
       </div>
       
