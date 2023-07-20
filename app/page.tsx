6b556da6ef3f9d8d7fecfeb7dbdd13c4154ad6c5
horn1347 <horn1347@users.noreply.github.com>
2023-07-20 21:04:01 +0200

    Basic layout noice

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export async function Blog() {
   console.log(entries);
 
   return (
-    <div className="flex flex-col">
-      <div>
-        <h2>
+    <div className="flex flex-col ">
+      <div className="p-2">
+        <h2 className="font-bold">
           Title
         </h2>
       </div>
-      <div>
-        <p>
+      <div className="p-2">
+        <p className=""> 
           Content
         </p>
       </div>
@@ -25,13 +25,13 @@ export async function Blog() {
 export function About() {
   return (
     <div className="flex flex-col">
-      <div>
+      <div className="p-2" font-bold>
         Whoami?
       </div>
-      <div>
+      <div className="p-2" font-bold>
         What am i doing here?
       </div>
-      <div>
+      <div className="p-2" font-bold>
         How to get in touch?
       </div>
     </div>
@@ -40,12 +40,12 @@ export function About() {
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-row">
-        <div id="blog" className="basis-2/3">
+    <main >
+      <div className="flex flex-row font-mono  p-4 m-4 text-green-600 bg-black  space-x-2">
+        <div id="blog" className="basis-2/3  border-dotted border-2 rounded-md border-inherit border-green-800">
           <Blog/>
         </div>
-        <div id="about" className="basis-1/2">
+        <div id="about" className="basis-1/2 border-dotted border-2 rounded-md border-green-800">
             <About/>
         </div>
       </div>
